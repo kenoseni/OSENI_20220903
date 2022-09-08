7d1ae90b46e82ff388d7c05f27147f8ad7886878
Olusola Oseni <kenolusola@gmail.com>
2022-09-08 23:01:28 +0100

    Update upload video form

--- client/src/components/pages/UploadForm.js
+++ client/src/components/pages/UploadForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import { Toast } from "../Toast";
 import { toast } from "react-toastify";
 import axios from "axios";
 // import { UploadOutlined } from "@ant-design/icons";
-import { Form, Button, Input, Select } from "antd";
+import { Form, Button, Input, Select, Progress, Spin } from "antd";
 import { useEffectOnlyOnce } from "../../hooks/useEffectOnce";
 import useRequest from "../../hooks/useRequest";
 
@@ -11,29 +12,45 @@ const { Option } = Select;
 axios.defaults.withCredentials = true;
 
 export const UploadForm = () => {
+  const [form] = Form.useForm();
   const [categories, setCategories] = useState([]);
   const [formData, setFormData] = useState({});
-  const [video, setVideo] = useState({});
+  const [video, setVideo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const onFinish = async (values) => {
+    setLoading(true);
     const { title, categoryId } = values;
     const data = new FormData();
-    data.append("title", title);
-    data.append("categoryId", categoryId);
-
     if (video) {
       data.append("video", video);
     }
+    data.append("title", title);
+    data.append("categoryId", categoryId);
+
     const response = await axios({
       method: "post",
       baseURL: `${process.env.REACT_APP_PROXY}/videos`,
-      headers: { "Content-Type": "multipart/form-data" },
       data,
+      headers: { "Content-Type": "multipart/form-data" },
+      onUploadProgress: (progressEvent) => {
+        setProgress(
+          parseInt(
+            Math.round((progressEvent.loaded / progressEvent.total) * 100)
+          )
+        );
+      },
     });
+
+    if (response) {
+      setLoading(false);
+      toast.success(response.data.data.message);
+    }
   };
 
   const onFinishFailed = (errorInfo) => {
-    toast(errorInfo);
+    toast.error(errorInfo);
   };
 
   const handleTitleChange = (e) => {
@@ -43,7 +60,7 @@ export const UploadForm = () => {
     setFormData({ ...formData, categoryId: value });
   };
   const handleFileChange = (e) => {
-    setVideo({ [e.target.name]: e.target.files[0] });
+    setVideo(e.target.files[0]);
   };
 
   const { doRequest, errors } = useRequest({
@@ -66,58 +83,70 @@ export const UploadForm = () => {
     }
   }, [categories, errors, doRequest]);
   return (
-    <Form
-      encType="multipart/form-data"
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Title"
-        name="title"
-        rules={[{ required: true, message: "Please input video title!" }]}
+    <>
+      <Form
+        encType="multipart/form-data"
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        form={form}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input onChange={handleTitleChange} />
-      </Form.Item>
-      <Form.Item
-        name="categoryId"
-        label="Category"
-        rules={[{ required: true }]}
-      >
-        <Select
-          placeholder="Select category"
-          onChange={onCategoryChange}
-          allowClear
+        <Form.Item
+          label="Title"
+          name="title"
+          rules={[{ required: true, message: "Please input video title!" }]}
         >
-          {categories &&
-            categories.map((category) => (
-              <Option value={category.id} key={category.id}>
-                {category.category}
-              </Option>
-            ))}
-        </Select>
-      </Form.Item>
-      <Form.Item
-        label="File"
-        name="file"
-        rules={[{ required: true, message: "Please choose mp4 or mov video" }]}
-      >
-        <Input
-          onChange={handleFileChange}
-          name="video"
-          type="file"
-          bordered={false}
-        />
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <Input onChange={handleTitleChange} />
+        </Form.Item>
+        <Form.Item
+          name="categoryId"
+          label="Category"
+          rules={[{ required: true }]}
+        >
+          <Select
+            placeholder="Select category"
+            onChange={onCategoryChange}
+            allowClear
+          >
+            {categories &&
+              categories.map((category) => (
+                <Option value={category.id} key={category.id}>
+                  {category.category}
+                </Option>
+              ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="File"
+          name="file"
+          rules={[
+            { required: true, message: "Please choose mp4 or mov video" },
+          ]}
+        >
+          <Input
+            onChange={handleFileChange}
+            name="video"
+            type="file"
+            bordered={false}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={loading ? true : false}
+          >
+            Submit
+          </Button>{" "}
+          {loading && <Spin />}
+        </Form.Item>
+      </Form>
+      {loading && <Progress percent={progress} status="active" />}
+      <Toast />
+    </>
   );
 };
